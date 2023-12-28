bbf2b48d357d02b2c9c31f718607d5c97522a29d
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2023-12-28 16:28:55 -0500

    route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,13 @@ export function Header() {
     <div className="text-4xl font-bold text-center my-5 text-white">
       <div className="flex justify-between items-start">
         <span>Seguimiento de gastos</span>
+        <a href="http://localhost:3000/index.html">
+        <input
+          type="button"
+          value="Salir"
+          className="bg-red-500 text-white px-3 py-1 rounded shadow-md text-sm self-start cursor-pointer"
+        />
+        </a>
       </div>
     </div>
   );
